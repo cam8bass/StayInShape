c698a9fe769156c271576025f78d099f0917b716
camille <lc.laignel@gmail.com>
2022-10-21 15:34:07 +0200

    add meta description

--- templates/layout.php
+++ templates/layout.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./public/css/main.css" />
+  <meta name="description" content="Stay in shape est une grande marque de salle de sport qui possède des franchises dans toute la France. Ici, vous pourrez accéder à son application de gestion de droit d’accès.">
   <title><?= $title ?></title>
 </head>
 
